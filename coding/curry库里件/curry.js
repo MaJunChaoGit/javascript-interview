217bbf171b342245b7814878f3ee831a3b2fb479
MaJunChaoGit <354488954@qq.com>
2018-11-10 11:26:10 +0800

    库里件 中间件代码实现

---
+++ coding/curry库里件/curry.js
@@ -0,0 +1,40 @@
+// 第一遍实现
+function curry(cb) {
+  let callback = cb;
+  let paramLength = cb.length;
+  let args = [];
+
+  function _curry(param) {
+    if (param) args.push(param); 
+
+    if (args.length === paramLength) {
+      let result = callback.apply(callback, args);
+      args.length = 0;
+      return result;
+    }      
+      return _curry;
+  }
+
+  return _curry
+}
+// 重构后
+function curry(cb) {
+  let args = [].slice.call(arguments, 1);
+  return function () {
+    const newArgs = args.concat(Array.from(arguments));
+
+    if (newArgs.length < cb.length) {
+      return curry(cb , ...newArgs);
+    } else {
+      return cb(...newArgs);
+    }
+  }
+}
+
+function add(a, b,c ,d) {
+    return a + b + c + d;
+}
+var curried = curry(add);
+console.log(curried(1)(2)(3)(100));
+
+// 106
---
+++ coding/middleware中间件/middleware.js
@@ -0,0 +1,48 @@
+// 初始版本
+function Middleware() {
+  let tasks = [];
+
+  this.use = function(fn) {
+
+    let self = this;
+
+    tasks.push(function() {
+      fn.call(self, self.next);
+    })
+  }
+
+  this.next = function() {
+    let fn = tasks.shift();
+    fn && fn();
+  }
+
+  this.go = function(fn) {
+    let self = this;
+    tasks.push(function() {
+      fn.call(self);
+    });
+    this.next();
+  }
+}
+
+// 重构版本
+function Middleware() {
+  let tasks = [];
+
+  this.use = function(fn) {
+    tasks.push(fn);
+    return this;
+  }
+
+  this.next = function() {
+    if (tasks.length > 0) {
+      let fn = tasks.shift();
+      fn && fn.call(this, this.next.bind(this));
+    }
+  }
+
+  this.go = function(fn) {
+    tasks.push(fn);
+    this.next();
+  }
+}
