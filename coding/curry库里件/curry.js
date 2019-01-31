0bc635c9e137a79a359abfd947227642415ce6d7
MaJunChaoGit <354488954@qq.com>
2019-01-31 23:51:39 +0800

    回忆了下函数科里化

--- coding/curry库里件/curry.js
+++ coding/curry库里件/curry.js
@@ -1,50 +1,62 @@
-// 第一遍实现
-function curry(cb) {
-  let callback = cb;
-  let paramLength = cb.length;
-  let args = [];
+// // 第一遍实现
+// function curry(cb) {
+//   let callback = cb;
+//   let paramLength = cb.length;
+//   let args = [];
 
-  function _curry(param) {
-    if (param) args.push(param); 
+//   function _curry(param) {
+//     if (param) args.push(param); 
 
-    if (args.length === paramLength) {
-      let result = callback.apply(callback, args);
-      args.length = 0;
-      return result;
-    }      
-      return _curry;
-  }
+//     if (args.length === paramLength) {
+//       let result = callback.apply(callback, args);
+//       args.length = 0;
+//       return result;
+//     }      
+//       return _curry;
+//   }
 
-  return _curry
-}
-// 重构后
-function curry(cb) {
-  let args = [].slice.call(arguments, 1);
-  return function () {
-    const newArgs = args.concat(Array.from(arguments));
+//   return _curry
+// }
+// // 重构后
+// function curry(cb) {
+//   let args = [].slice.call(arguments, 1);
+//   return function () {
+//     const newArgs = args.concat(Array.from(arguments));
 
-    if (newArgs.length < cb.length) {
-      return curry(cb , ...newArgs);
-    } else {
-      return cb(...newArgs);
-    }
-  }
-}
+//     if (newArgs.length < cb.length) {
+//       return curry(cb , ...newArgs);
+//     } else {
+//       return cb(...newArgs);
+//     }
+//   }
+// }
 
-function add(a, b,c ,d) {
-    return a + b + c + d;
-}
-var curried = curry(add);
-console.log(curried(1)(2)(3)(100));
+// function add(a, b,c ,d) {
+//     return a + b + c + d;
+// }
+// var curried = curry(add);
+// console.log(curried(1)(2)(3)(100));
 
-// 106
-// 
+// // 106
+// // 
+
+// function curry(cb) {
+//   let args = [].slice.call(arguments, 1);
+//   return function() {
+//     const newArgs = args.concat(Array.from(arguments));
+
+//     if (newArgs.length < cb.length) {
+//       return curry(cb, ...newArgs);
+//     } else {
+//       return cb(...newArgs);
+//     }
+//   }
+// }
 
 function curry(cb) {
   let args = [].slice.call(arguments, 1);
   return function() {
     const newArgs = args.concat(Array.from(arguments));
-
     if (newArgs.length < cb.length) {
       return curry(cb, ...newArgs);
     } else {
